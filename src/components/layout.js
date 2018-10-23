1ea4c21523d96e431aacd31a055c4e4e6251e3ff
struct78 <david@struct.com.au>
2018-10-24 09:26:12 +1100

    Fix syntax error

--- src/components/layout.js
+++ src/components/layout.js
@@ -38,7 +38,7 @@ const Layout = ({ children }) => (
                 google: {
                   families: ['Rajdhani:100,400', 'Roboto Slab:100,200,300,400'],
                 }
-              }
+              };
               (function(d) {
                   var wf = d.createElement('script'), s = d.scripts[0];
                   wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
